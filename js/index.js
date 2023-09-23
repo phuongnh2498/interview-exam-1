265f0ca8794724d252657fd9a98292f330438848
phuongnh2498 <phuongnh2498@gmail.com>
2023-09-24 00:44:23 +0700

    feature(root): add js to scroll sections

--- js/index.js
+++ js/index.js
@@ -1,28 +1,102 @@
-const header = document.getElementById("header-nav");
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSectionIndex = 0;
+  const pageNumbers = document.querySelectorAll(".page-number");
+  const navItems = document.querySelectorAll(".nav-item");
+  const sections = document.querySelectorAll("section");
+  const prevButton = document.getElementById("prev");
+  const nextButton = document.getElementById("next");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    // Add a background color when scrolling down
-    header.classList.add("sticky-nav");
-  } else {
-    // Reset to the initial background color when at the top
-    header.classList.remove("sticky-nav");
-  }
-});
+  function setActiveSection(index, manually = false) {
+    if (!manually) {
+      sections.forEach((section, i) => {
+        if (i === index) {
+          section.scrollIntoView({ behavior: "smooth" }); // Scroll to the active section
+        }
+      });
+    }
 
-let list_id_nav = ["nav1", "nav2", "nav3", "nav4", "nav5"];
-let cur_nav_state = "nav1";
-
-const navItems = document.querySelectorAll(".nav-item");
-
-navItems.forEach((item) => {
-  item.addEventListener("mouseenter", () => {
-    // Remove the "active" class from all items
-    navItems.forEach((navItem) => {
-      navItem.classList.remove("nav-active");
+    pageNumbers.forEach((pageNumber, i) => {
+      if (i === index) {
+        pageNumber.classList.add("page-number-active");
+      } else {
+        pageNumber.classList.remove("page-number-active");
+      }
     });
 
-    // Add the "active" class to the currently hovered item
-    item.classList.add("nav-active");
+    navItems.forEach((pageNumber, i) => {
+      if (i === index) {
+        pageNumber.classList.add("nav-active");
+      } else {
+        pageNumber.classList.remove("nav-active");
+      }
+    });
+  }
+
+  navItems.forEach((pageNumber, index) => {
+    pageNumber.addEventListener("click", () => {
+      currentSectionIndex = index;
+      setActiveSection(currentSectionIndex);
+    });
+  });
+
+  pageNumbers.forEach((pageNumber, index) => {
+    pageNumber.addEventListener("click", () => {
+      currentSectionIndex = index;
+      setActiveSection(currentSectionIndex);
+    });
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (currentSectionIndex > 0) {
+      currentSectionIndex--;
+      setActiveSection(currentSectionIndex);
+    }
+  });
+
+  nextButton.addEventListener("click", () => {
+    if (currentSectionIndex < sections.length - 1) {
+      currentSectionIndex++;
+      setActiveSection(currentSectionIndex);
+    }
+  });
+
+  // manually scroll observer
+  function handleIntersection(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const sectionId = entry.target.getAttribute("id");
+        const index = Array.from(sections).findIndex(
+          (section) => section.getAttribute("id") === sectionId
+        );
+        if (index !== -1) {
+          currentSectionIndex = index;
+          setActiveSection(currentSectionIndex, true);
+        }
+      }
+    });
+  }
+
+  // Create an Intersection Observer to track section intersections
+  const observer = new IntersectionObserver(handleIntersection, {
+    root: null, // viewport
+    rootMargin: "0px",
+    threshold: 0.5, // 50% of the section needs to be visible
+  });
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+
+  //add scroll behavior
+  const header = document.getElementById("header-nav");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      // Add a background color when scrolling down
+      header.classList.add("sticky-nav");
+    } else {
+      // Reset to the initial background color when at the top
+      header.classList.remove("sticky-nav");
+    }
   });
 });
